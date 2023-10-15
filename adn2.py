e1c53074b0c30e428644e934057d64165e73ea30
A01029303 <141659231+A01029303@users.noreply.github.com>
2023-10-15 00:58:18 +0000

    10

--- adn2.py
+++ adn2.py
@@ -3,6 +3,14 @@ from pathlib import Path
 import sys
 
 def lectura_de_archivos_base_de_datos(ruta_b):
+    """_Método que hace la lectura de la base de datos y regresa los datos en lista_
+
+    Args:
+        ruta_b (_pathlib.PosixPath_): _Dirección de los archivo que se van a leer_
+
+    Returns:
+        _list_: _Datos del databases sobre los STR y las personas_
+    """
     f=open(ruta_b) #Para abrir la base de datos
     base_de_datos=[]
     for linea in f:
@@ -26,6 +34,14 @@ def lectura_de_archivos_base_de_datos(ruta_b):
     return lista_resultante
 
 def lectura_de_secuencias(ruta_s):
+    """_Método que hace la lectura de las secuencias_
+
+    Args:
+        ruta_s (_pathlib.PosixPath_): _Dirección de los archivos de secuencias que se van a leer_
+
+    Returns:
+        _list_: _Secuencias de DNA que se van a comparar_
+    """
     f1=open(ruta_s)
     secuencia= []
     for linea1 in f1:
@@ -35,6 +51,15 @@ def lectura_de_secuencias(ruta_s):
     return secuencia
 
 def compara_str(base_datos,buscarstr):
+    """_Método que compara la cantidad de STR con las personas y si no hay nadie igual False_
+
+    Args:
+        base_datos (_list_): _Datos del databases sobre los STR y las personas_
+        buscarstr (_list_): _Cantidad de veces que se repite cada STR_
+
+    Returns:
+        _string_: _El nombre de la persona con la que coincide el STR y secuencia_
+    """
     for k in range(len(base_datos)): 
         nombre= base_datos[k][0]
         numeros= base_datos[k][1:]
@@ -42,8 +67,19 @@ def compara_str(base_datos,buscarstr):
             return (nombre)
             
     return (False)
-    
+
+
 def buscar_str(secuencias,strprueba):
+    """_Método que cuenta la cantidad de veces que se repite cada STR en la cadena_
+
+    Args:
+        secuencias (_list_): _Secuencias de DNA que se van a comparar_
+        strprueba (_list_): _STR que se va a buscar en la secuencia_
+
+    Returns:
+        _int_: _Cantidad de veces seguidas que se repite el STR en la secuencia_
+    """
+    
     contar_secuencia = secuencias[0] #AAGGTAAGTTTAGAATATAAAAGGTGAGTTAAA
     lista_secuencia = list(contar_secuencia) #['A', 'A', 'G', 'G', 'T', 'A',.........]
     
@@ -70,6 +106,14 @@ def buscar_str(secuencias,strprueba):
     return repeticiones
 
 def buscar_str2(base_datos):
+    """_Método que busca los STR que están en la base de datos y que van a ser usados para comparar_
+
+    Args:
+        base_datos (_list_): _Datos sobre todos los STR y repeticiones de cada persona_
+
+    Returns:
+        _list_: _STR que se va a buscar en la secuencia_
+    """
     strprueba = []
     for i in range (1,len(base_datos[0] )): #[0][1]
         elemento= base_datos[0][i]
